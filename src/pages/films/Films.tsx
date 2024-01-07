3e29043475ba77326f83d86e6da96ce0336bec0f
Alexander03e <cfifeg1@gmail.com>
2024-01-07 22:23:13 +0300

    module1 + module2

--- src/pages/films/Films.tsx
+++ src/pages/films/Films.tsx
@@ -4,20 +4,27 @@ import React from 'react'
 import Button from 'src/components/Button/Button'
 import Movie from 'src/components/MovieList/Movie/Movie'
 import styled from 'styled-components'
+import { Table } from 'antd';
+import type {ColumnsType} from 'antd/es/table'
+
+interface Film {
+  title: string,
+  subtitle:string, 
+  year:number,
+  genre:string[],
+  rating_stars:number,
+  id:number,
+  image:string,
+  is_favorite: boolean 
+}
+
+interface StyledProps {
+  Color?: string
+}
 
 const Films = () => {
-    
-  interface FilmType {
-    title: string,
-    subtitle:string, 
-    year:number,
-    genre:string[],
-    id:number,
-    image:string,
-    is_favorite: boolean 
-  }
 
-  const [films,setFilms] = React.useState<FilmType | ''>('')
+  const [films,setFilms] = React.useState<Film[]>([])
   const [curPage, setCurPage] = React.useState<number>(0)
   const [filters, setFilters] = React.useState(false)
   const [initFilms, setInitFilms] = React.useState('')
@@ -27,6 +34,34 @@ const Films = () => {
     .then((res)=>{setFilms([...films, ...res.data.results]), setInitFilms([...films, ...res.data.results])})
   } 
   
+  const columns : ColumnsType<Film> = [
+    {
+      key: 'title',
+      title: 'Фильм',
+      dataIndex: 'title',
+    }, 
+    {
+      key:'subtitle',
+      title: 'Описание',
+      dataIndex: 'subtitle'
+    }, 
+    {
+      key: 'rating',
+      title: 'Рейтинг',
+      dataIndex: 'rating_stars'
+    },
+    {
+      key:'year',
+      title: 'Год',
+      dataIndex: 'year',
+    },
+    {
+      key:'genre',
+      title: 'Жанр',
+      dataIndex: 'genre'
+    }
+  ]
+
   let filmsURL = `http://127.0.0.1:8000/api/v1/films/?offset=${curPage*6}&limit=6`
   React.useEffect(() => {
     getFilms()
@@ -37,8 +72,8 @@ const Films = () => {
     setCurPage(1)
   }
 
-  const Filter = styled.p`
-  color: var(--text-color);
+  const Filter = styled.p<StyledProps>`
+  color: ${({Color}) => Color ? Color: 'red'};
   text-decoration:underline;
   cursor:pointer;
   &:focus{
@@ -85,9 +120,14 @@ const Films = () => {
   return (
     <section className="films">
       <div className="container">
+        <Table<Film> columns={columns} dataSource={films}/>
+        <Button text='Загрузить еще' onClick={handleClick}/>
+
         <h1 style={{color:'var(--text-color)'}}>Фильмы</h1>
         <p>Сортировка:</p>
-        <Filter onClick={openFilters}>Жанр</Filter>
+        
+        <Filter Color="var(--text-color)" onClick={openFilters}>Жанр</Filter> {/* interface StyledProps */}
+
         {filters && <FilterWrapper><Filter onClick={changeGenreFilter} tabindex='0'>Боевик</Filter><Filter onClick={changeGenreFilter1} tabindex='0'>Фантастика</Filter></FilterWrapper>}
         <div className="films-wrapper">
           {films != ''?films.map(film=> <Movie key={film.id} film={film}/>):''}
