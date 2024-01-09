fd9b0c4fe1ea642902c460ce9c0593a463a70491
Alexander03e <cfifeg1@gmail.com>
2024-01-09 18:06:46 +0300

    remake module 1_2_4

--- src/pages/films/Films.tsx
+++ src/pages/films/Films.tsx
@@ -22,18 +22,49 @@ interface StyledProps {
   Color?: string
 }
 
+interface User {
+  key: number;
+  name: string;
+  age: number;
+}
+
 const Films = () => {
 
   const [films,setFilms] = React.useState<Film[]>([])
+  const [dataSource, setDataSource] = React.useState<User[]>([])
   const [curPage, setCurPage] = React.useState<number>(0)
+  const [page, setPage] = React.useState<number>(1)
   const [filters, setFilters] = React.useState(false)
   const [initFilms, setInitFilms] = React.useState('')
+  const LIMIT_LIST_USERS = 8;
+  const skip = (page - 1) * LIMIT_LIST_USERS;
+  
+  const getUsers = async (skip:number, limit:number) => {
+    await axios
+      .get(`https://dummyjson.com/users?limit=${limit}&skip=${skip}`)
+      .then(res => setDataSource(res.data.users))
+  }
+
   const getFilms = async () => {
     await axios
     .get(filmsURL)
     .then((res)=>{setFilms([...films, ...res.data.results]), setInitFilms([...films, ...res.data.results])})
   } 
-  
+
+  const columns_user: ColumnsType<User> = [
+    {
+      key: 'name',
+      title: 'Имя',
+      dataIndex: 'firstName',
+      render: (text) => <a>{text}</a>,
+    },
+    {
+      key: 'age',
+      title: 'Возраст',
+      dataIndex: 'age',
+    },
+  ]
+
   const columns : ColumnsType<Film> = [
     {
       key: 'title',
@@ -65,8 +96,8 @@ const Films = () => {
   let filmsURL = `http://127.0.0.1:8000/api/v1/films/?offset=${curPage*6}&limit=6`
   React.useEffect(() => {
     getFilms()
-    
-  }, [curPage])
+    getUsers(skip, LIMIT_LIST_USERS)
+  }, [page])
 
   const handleClick = () => {
     setCurPage(1)
@@ -120,7 +151,11 @@ const Films = () => {
   return (
     <section className="films">
       <div className="container">
-        <Table<Film> columns={columns} dataSource={films}/>
+        <Table<Film> columns={columns_user} dataSource={dataSource}/>
+        <button onClick={() => setPage(page - 1)} disabled={!skip}>Назад</button>
+        <button onClick={() => setPage(page + 1)}>Вперед</button>
+        <p>Текущая страница: {page}</p>
+
         <Button text='Загрузить еще' onClick={handleClick}/>
 
         <h1 style={{color:'var(--text-color)'}}>Фильмы</h1>
